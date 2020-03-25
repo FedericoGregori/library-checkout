f2025d5648363de62ec31b17d4ae90ef00586f7e
FedericoGregori <jfedericogregori@gmail.com>
2020-03-25 03:20:05 -0300

    [ADD] Form view Features

--- models/library_checkout.py
+++ models/library_checkout.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, exceptions
 
 
 class Checkout(models.Model):
@@ -6,23 +6,22 @@ class Checkout(models.Model):
     _description = 'Checkout Request'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    @api.multi
-    def action_open(self):
-        self.ensure_one()
-        for rec in self:
-            rec.state = 'open'
+    _rec_name = 'member_id'
 
-    @api.multi
-    def action_done(self):
-        self.ensure_one()
-        for rec in self:
-            rec.state = 'done'
+    def button_done(self):
+        stages = self.env['library.checkout.stage']
+        done_stage = stages.search([('state', '=', 'done')], limit=1)
+        for checkout in self:
+            checkout.stage_id = done_stage
+        return True
 
     member_id = fields.Many2one(
         'library.member',
         'Library Member',
         required=True
     )
+
+    member_image = fields.Binary(related='member_id.partner_id.image')
 
     user_id = fields.Many2one(
         'res.users',
@@ -33,28 +32,48 @@ class Checkout(models.Model):
     request_date = fields.Date(
         default=lambda s: fields.Date.today())
 
+    checkout_date = fields.Date(readonly=True)
+    closed_date = fields.Date(readonly=True)
+
+    @api.model
+    def create(self, vals):
+        # Code before create: should use the `vals` dict
+        if 'stage_id' in vals:
+            Stage = self.env['library.checkout.stage']
+            new_state = Stage.browse(vals['stage_id']).state
+            if new_state == 'open':
+                vals['checkout_date'] = fields.Date.today()
+
+        new_record = super().create(vals)
+
+        # Code after create: can use the `new_record` created
+        if new_record.state == 'done':
+            raise exceptions.UserError(
+                'Not allowed to create a checkout in the done state.')
+
+        return new_record
+
     line_ids = fields.One2many(
         'library.checkout.line',
         'checkout_id',
         string='Borrowed Books'
     )
 
-    # to add in the class Checkout:
     @api.model
     def _default_stage(self):
-        Stage = self.env['library.checkout.stage'].search([], limit=1)
-        return Stage
+        stage = self.env['library.checkout.stage'].search([], limit=1)
+        return stage
 
     @api.model
     def _group_expand_stage_id(self, stages, domain, order):
         return stages.search([], order=order)
 
     stage_id = fields.Many2one(
-     'library.checkout.stage',
-     default=_default_stage,
-     group_expand='_group_expand_stage_id')
+        'library.checkout.stage',
+        default=_default_stage,
+        group_expand='_group_expand_stage_id')
 
-    state = fields.Selection(related='stage_id.state', store=True)
+    state = fields.Selection(related='stage_id.state')
 
     @api.onchange('member_id')
     def onchange_member_id(self):
@@ -68,11 +87,22 @@ class Checkout(models.Model):
                 }
             }
 
+    # Smart Button
+    num_other_checkouts = fields.Integer(compute='_compute_num_other_checkouts')
+
+    def _compute_num_other_checkouts(self):
+        for rec in self:
+            domain = [
+                ('member_id', '=', rec.member_id.id),
+                ('state', 'in', ['open']),
+                ('id', '!=', rec.id)
+            ]
+            rec.num_other_checkouts = self.search_count(domain)
+
 
 class CheckoutLine(models.Model):
     _name = 'library.checkout.line'
     _description = 'Borrow Request Line'
-    _rec_name = 'book_id'
 
     checkout_id = fields.Many2one('library.checkout')
     book_id = fields.Many2one('library.book')
